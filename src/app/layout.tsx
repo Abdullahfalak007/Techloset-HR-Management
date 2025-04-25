307c766e6a585a3641ead82c5d5b40f7d19728df
Muhammad Abdullah <abdullahfalak007@gmail.com>
2025-04-25 16:16:36 +0500

    Proceeding after the authentication!

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,3 @@
-// "use client";
-
-// import "./globals.css";
-// import { SessionProvider } from "next-auth/react";
-// import StoreProvider from "@/app/StoreProvider"; // your redux provider
-
-// export default function RootLayout({
-//   children,
-// }: {
-//   children: React.ReactNode;
-// }) {
-//   return (
-//     <html lang="en">
-//       <body>
-//         <SessionProvider>
-//           <StoreProvider>{children}</StoreProvider>
-//         </SessionProvider>
-//       </body>
-//     </html>
-//   );
-// }
-
 "use client";
 
 import "./globals.css";
